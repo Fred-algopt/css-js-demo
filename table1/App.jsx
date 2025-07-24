85b6e577642c8a09d2c2735190950efbb7e94606
Fred-algopt <algopt.fr@gmail.com>
2025-07-24 14:01:56 +0200

    Create App.jsx

---
+++ table1/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dashboard from './Dashboard.jsx';
+
+ReactDOM.render(
+  <Dashboard classes={{
+    tableContainer: '',
+    appBarSpacer: '',
+    container: '',
+    drawerPaper: '',
+    title: '',
+    toolbar: '',
+    root: ''
+  }} />,
+  mountNode);
+  
+  HTMLAnchorElement.appBarSpacer.Dashboard
+  NavigationPreloadManager.appBarSpacer.container
+  matchMedia.appBarSpacer.Dashboard
+  mountNode.accessKey
+  menubar.container
+  NavigationPreloadManager.apply
+  name.apply.classes
